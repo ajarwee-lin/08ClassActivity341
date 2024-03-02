25586a74096e1e150d392df05c7b005e20d4e853
A. Lincoln Jarwee <122073281+ajarwee-lin@users.noreply.github.com>
2024-03-02 14:16:25 -0700

    commit new changes

--- server.js
+++ server.js
@@ -1,17 +1,12 @@
-const express = require('express');
-const app = express();
-const { auth, requiresAuth } = require('express-openid-connect');
-require('dotenv').config();
-
-const port = process.env.PORT || 3000;
+const { auth } = require('express-openid-connect');
 
 const config = {
   authRequired: false,
   auth0Logout: true,
-  secret: process.env.SECRET,
-  baseURL: process.env.BASE_URL,
-  clientID: process.env.CLIENT_ID,
-  issuerBaseURL: process.env.ISSUER_BASE_URL,
+  secret: 'a long, randomly-generated string stored in env',
+  baseURL: 'http://localhost:3000',
+  clientID: 'sjFZnCsdcXtj7Stao1Ui8sAgj9EFlxy6',
+  issuerBaseURL: 'https://dev-55jpv4zapj70w68l.us.auth0.com'
 };
 
 // auth router attaches /login, /logout, and /callback routes to the baseURL
@@ -20,12 +15,4 @@ app.use(auth(config));
 // req.isAuthenticated is provided from the auth router
 app.get('/', (req, res) => {
   res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
-});
-
-app.get('/profile', requiresAuth(), (req, res) => {
-  res.send(JSON.stringify(req.oidc.user));
-});
-
-app.listen(port, () => {
-  console.log(`listening on port ${port}`);
 });
