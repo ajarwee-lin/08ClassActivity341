a63024221f3192196b21dd671ae1ffc39351262f
A. Lincoln Jarwee <122073281+ajarwee-lin@users.noreply.github.com>
2024-03-02 14:52:42 -0700

    commit

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const express = require('express');
 const app = exress();
 const { auth } = require('express-openid-connect');
 require ('dotenv').config();
@@ -5,10 +6,10 @@ require ('dotenv').config();
 const config = {
   authRequired: false,
   auth0Logout: true,
-  secret: process.eventNames.SECRET,
-  baseURL: process.eventNames.BASE_URL,
+  secret: process.env.SECRET,
+  baseURL: process.env.BASE_URL,
   clientID: process.env.CLIENT_ID,
-  issuerBaseURL: process.eventNames.ISSUER_BASE_URL,
+  issuerBaseURL: process.env.ISSUER_BASE_URL,
 };
 
 // auth router attaches /login, /logout, and /callback routes to the baseURL
